bc38cf79297c62e8f513b271eb3b727acf52a2dc
GrayViper <chakkaanil6@gmail.com>
2026-03-03 11:24:58 +0530

    Second commit

--- tasks.py
+++ tasks.py
@@ -1,3 +1,3 @@
-tasks = [] def display_tasks(): if not tasks: print("[task]No tasks yet.") for i, task in enumerate(tasks, 1): print(f" {i}. {task}")
+tasks = [] def display_tasks(): if not tasks: print("[TASK]No tasks yet.") for i, task in enumerate(tasks, 1): print(f" {i}. {task}")
 
 def delete_task(task_index): if 0 <= task_index < len(tasks): removed = tasks.pop(task_index) print(f"🗑️ Removed: {removed}") else: print("Invalid task index.")
