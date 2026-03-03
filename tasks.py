e9de321d1cd98fde37a660b92a65aee74bc49457
Zlan9 <archishmansingh805@gmail.com>
2026-03-03 11:16:15 +0530

    New branch added

--- tasks.py
+++ tasks.py
@@ -1,1 +1,2 @@
 tasks = [] def display_tasks(): if not tasks: print("No tasks yet.") for i, task in enumerate(tasks, 1): print(f" {i}. {task}")
+def add_task(task_name): tasks.append(task_name) print(f"✅ Task added: {task_name}")
